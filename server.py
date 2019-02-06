c4944741b7f92bb14b3b10e1d6ca7684ee445a30
Grant Parton <granteparton@gmail.com>
2019-02-06 12:35:55 -0800

    Now adds the agent of the computer to a logfile

--- server.py
+++ server.py
@@ -1,18 +1,19 @@
-from flask import Flask, render_template, request           
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-# Logging file
-logFile = open("logFile.txt", "w")
 
 @app.route("/")
 def root():
     return render_template("root.html")
 
+
 @app.route('/img/<img_name>')
 def show_post(img_name):
     # show the post with the given id, the id is an integer
-    #return 'Img %s' % img_name
+    # return 'Img %s' % img_name
     print(request.headers)
+    with open("logFile.txt", "w+") as logger:
+        logger.write(request.headers["user-agent"] + "\n")
     return render_template("root.html")
 
 if __name__ == "__main__":
