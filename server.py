cbd9b5b417543105d9e904fb4f63054c2842937e
Grant Parton <granteparton@gmail.com>
2019-02-06 11:52:16 -0800

    Sends get request on clicking the top left image, just prints to console right now.

--- server.py
+++ server.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template             
-
+from flask import Flask, render_template, request           
 app = Flask(__name__)
+
+# Logging file
+logFile = open("logFile.txt", "w")
 
 @app.route("/")
 def root():
+    return render_template("root.html")
+
+@app.route('/img/<img_name>')
+def show_post(img_name):
+    # show the post with the given id, the id is an integer
+    #return 'Img %s' % img_name
+    print(request.headers)
     return render_template("root.html")
 
 if __name__ == "__main__":
