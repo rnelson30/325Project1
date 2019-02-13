8e47072f28483dc6eb631ee0ef752b6d10c1e4dd
Nick Papadakis <nicholas.t.papadakis@gmail.com>
2019-02-13 12:13:29 -0800

    Log file now parses complete header, ~ delimited

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
@@ -12,8 +13,17 @@ def show_post(img_name):
     # show the post with the given id, the id is an integer
     # return 'Img %s' % img_name
     print(request.headers)
-    with open("logFile.txt", "w+") as logger:
-        logger.write(request.headers["user-agent"] + "\n")
+    with open("logFile.txt", "a") as logger:
+	logger.write(img_name + "~")
+	logger.write('%s~' % datetime.datetime.now())
+        logger.write(request.headers["user-agent"] + "~")
+        logger.write(request.headers["connection"] + "~")
+        logger.write(request.headers["dnt"] + "~")
+        logger.write(request.headers["host"] + "~")
+        logger.write(request.headers["upgrade-insecure-requests"] + "~")
+        logger.write(request.headers["accept"] + "~")
+        logger.write(request.headers["accept-language"] + "~")
+        logger.write(request.headers["accept-encoding"] + "\n")
     return render_template("root.html")
 
 if __name__ == "__main__":
