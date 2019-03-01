614e0d7a7ea111ed1c812a67079d2197e4d438cf
Ryan Nelson <Ryan.Nelson@ibm.com@Ryans-MacBook-Pro.local>
2019-02-28 17:22:21 -0800

    added differential privacy script

---
+++ noiseGen.py
@@ -0,0 +1,30 @@
+from random import randint
+import csv
+
+with  open('logFile.csv', 'r') as datafile:
+   datareader = csv.reader(datafile, delimiter='~')
+   data = []
+   noiseData = []
+   for row in datareader:
+      data.append(row) 
+
+   rows = len(data)      # get number of rows
+   cols = len(data[0])   # get number of cols
+
+   for row in data:
+      if(randint(0, 1) == 0):    # flip a coin
+         noiseData.append(row)   # 0: add original row to new dataset 
+      else:
+         noiseRow = []           # 1: add noise row
+         for i in range(cols):            # loop through column index
+            r = randint(0,rows-2)         # get random row in initial dataset
+            noiseRow.append(data[r][i])   # append row,col combination to noise row
+         noiseData.append(noiseRow)       # add noise row to new dataset
+
+   with open('noiseLogFile.csv', 'w') as writeFile:  # make new csv
+      writer = csv.writer(writeFile)
+      writer.writerows(noiseData)       
+
+   datafile.close()
+   writeFile.close()
+
